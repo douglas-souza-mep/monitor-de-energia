736180369b5064005f01d688b02cb9640b550be7
douglas-souza-mep <douglas.souza@mep.eng.br>
2023-11-22 19:34:17 -0300

    acesso ao bd

--- models/connection.js
+++ models/connection.js
@@ -4,9 +4,9 @@ require('dotenv').config()
 //console.log(process.env.MUSQL_USER)
 const connection = mysql.createPool({
     //user: 'root',
-    user: process.env.MYSQL_USER,
-    host: process.env.MYSQL_HOST,
-    password: process.env.MYSQL_PASSWORD,
+    user: 'admin', //process.env.MYSQL_USER,
+    host: 'localhost', //process.env.MYSQL_HOST,
+    password:'', //process.env.MYSQL_PASSWORD,
     //database: process.env.MYSQL_DB
     database: "medidores_de_energia"
   });
