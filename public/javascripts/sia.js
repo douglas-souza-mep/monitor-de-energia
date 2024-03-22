6e6776af1ca5db53fd464a86144b2a823ca5f89e
douglas-souza-mep <douglas.souza@mep.eng.br>
2024-03-22 15:14:55 -0300

    fator de potencia

--- public/javascripts/sia.js
+++ public/javascripts/sia.js
@@ -93,6 +93,7 @@ socket.on("atualizar_sia108",dados =>{
 })
 socket.on("atualizar_sia114",dados =>{
   if(dados.leitura.id == medidor){
+    console.log(dados.leitura)
     $('#data').text(dados.leitura.data)
     $('#va').text(dados.leitura.uarms + " V" ) 
     $('#vb').text(dados.leitura.ubrms + " V" )
