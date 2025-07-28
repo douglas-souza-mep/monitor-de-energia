fd149639bc34842710bc516b9bee8f99e4303660
Douglas Souza <douglas.souza@mep.eng.br>
2025-07-28 14:39:52 -0300

    popup de senha errada

--- routes/index.js
+++ routes/index.js
@@ -15,17 +15,10 @@ router.post('/', async(req,res)=>{
   let retorno= await Logar.logar(req, res)
   if (retorno.acesso==1) {
     res.redirect(retorno.url)
-  } else {
-    mostrarPopup(retorno.msg);
   }
 })
 
-function mostrarPopup(mensagem) {
-  const popup = document.getElementById("popup");
-  popup.textContent = mensagem;
-  popup.style.display = "block";
-  setTimeout(() => popup.style.display = "none", 3000); // some em 3s
-}
+
 
 router.post('/app/login', async (req, res) => {
   const { username, password } = req.body;
