73caf75045954ae166a8f8ec9c5519dae74c1941
Douglas Souza <douglas.souza@mep.eng.br>
2026-03-16 17:44:08 -0300

    segurança contra scaners

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 
 var indexRouter = require('./routes/index');
@@ -13,6 +14,24 @@ var indexRouterV2 = require('./routes/index_v2');
 const { subscribeToMqttTopics } = require('./mqtt-subscription');
 
 var app = express();
+
+app.set('trust proxy', 1);
+
+// 3. CONFIGURAÇÃO DO BLOQUEIO DE SCANNER (404)
+const scannerLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // Janela de 5 minutos
+  max: 10, // Limite de 10 erros 404 por IP
+  handler: (req, res, next) => {
+    console.warn(`[SEGURANÇA] IP Bloqueado por excesso de 404: ${req.ip}`);
+    globalThis.bot.telegram.sendMessage(process.env.CHAT_ID_DEV, `[SEGURANÇA] IP Bloqueado por excesso de 404: ${req.ip}`);
+    res.status(429).render('error', {
+      message: 'Acesso bloqueado temporariamente por comportamento suspeito.',
+      error: { status: 429 }
+    });
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 //app.io = require('socket.io')();
 
@@ -32,6 +51,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/', indexRouterV2);
 app.use('/users', require('./routes/users')(app.io));
+
+app.use(scannerLimiter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
